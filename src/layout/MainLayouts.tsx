8703f89bdc8af1687c8e6021f1c43618932e7150
Parvez Hossien Tarafder <parveztarafder31@gmail.com>
2026-06-30 14:04:41 +0530

    add MainLayouts component with Navbar and Outlet

---
+++ src/layout/MainLayouts.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const MainLayouts = () => {
+  return (
+    <>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+
+        <main className="max-w-5xl mx-auto px-4">
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default MainLayouts;
